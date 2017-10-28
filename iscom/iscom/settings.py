cb162227ed3263e0da2e749d9b4881ae6c1c119c
davidtnfsh <davidtnfsh@gmail.com>
2017-10-28 15:02:34 +0000

    update settings.py

--- iscom/iscom/settings.py
+++ iscom/iscom/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'iscom.spiders'
 #USER_AGENT = 'iscom (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -27,9 +27,9 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
