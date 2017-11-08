4796fa19513ab9d13a32c33a448a16d106b60dbf
david <davidtnfsh@gmail.com>
2017-11-08 15:45:04 +0800

    use hybrid to make weight of distance outweighted of correlation, make result much better

--- iscom.py
+++ iscom.py
@@ -1,5 +1,5 @@
 from KCM.__main__ import KCM
-import json, sys, requests
+import json, sys, requests, os, pyprind
 k = KCM('cht', './ptt', uri='mongodb://172.17.0.11:27017')
 # k.removeDB()
 # k.main()
@@ -10,9 +10,13 @@ query = sys.argv[1]
 tmp = [i for i in k.get(query, 10000) if i[0] in name and len(i[0]) > 2 ][:int(sys.argv[2])]
 
 distance = []
-for i in tmp:
+for i in pyprind.prog_bar(tmp):
     try:
-        res = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins={}&destinations={}&key=AIzaSyB20qKjF1ePtq9t1luvFd-433J41anlDGU'.format(name2add[query], name2add[i[0]])).json()
+        if os.path.isfile(i[0]+'.json'):
+            res = json.load(open(i[0]+'.json','r'))
+        else:
+            res = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins={}&destinations={}&key=AIzaSyB20qKjF1ePtq9t1luvFd-433J41anlDGU'.format(name2add[query], name2add[i[0]])).json()
+            json.dump(res, open(i[0]+'.json','w'))
         if res['rows'][0]['elements'][0]['status'] == 'OK':
             distance.append((i[0], res['rows'][0]['elements'][0]['distance']['value']))
     except Exception as e:
@@ -21,12 +25,12 @@ distance = sorted(distance, key=lambda x:x[1])[:int(sys.argv[3])]
 print(tmp[:int(sys.argv[3])])
 print(distance)
 
-hybrid = []
-for i, j in zip(tmp, distance):
+hybrid = {}
+for index, (i, j) in enumerate(zip(tmp, distance)):
     if i[0] == j[0]:
-        hybrid.append(i)
+        hybrid[i[0]] = hybrid.setdefault(i[0], 0) + index
     else:
-        hybrid.append(i)
-        hybrid.append(j)
+        hybrid[i[0]] = hybrid.setdefault(i[0], 0) + index*0.01
+        hybrid[j[0]] = hybrid.setdefault(j[0], 0) + index*0.99
 
-print(hybrid[:int(sys.argv[3])*2])
+print(sorted(hybrid.items(), key=lambda x:-x[1])[:int(sys.argv[3])])
