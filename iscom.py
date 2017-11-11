3fe0a6508a6f14286cfe38231ac2815a4ae68cd7
davidtnfsh <davidtnfsh@gmail.com>
2017-11-11 09:12:57 +0000

    add kkday crawler + update iscom.py query script

--- iscom.py
+++ iscom.py
@@ -3,20 +3,19 @@ import json, sys, requests, os, pyprind
 k = KCM('cht', './ptt', uri='mongodb://172.17.0.11:27017')
 # k.removeDB()
 # k.main()
-name = json.load(open('name.json','r'))
 name2add = json.load(open('台灣所有景點的地址.json', 'r'))
 
 query = sys.argv[1]
-tmp = [i for i in k.get(query, 10000) if i[0] in name and len(i[0]) > 2 ][:int(sys.argv[2])]
+tmp = [i for i in k.get(query, 10000) if i[0] in name2add and len(i[0]) > 2 ][:int(sys.argv[2])]
 
 distance = []
 for i in pyprind.prog_bar(tmp):
     try:
-        if os.path.isfile(i[0]+'.json'):
-            res = json.load(open(i[0]+'.json','r'))
+        if os.path.isfile('json/' + i[0]+'.json'):
+            res = json.load(open('json/' + i[0]+'.json','r'))
         else:
             res = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins={}&destinations={}&key=AIzaSyB20qKjF1ePtq9t1luvFd-433J41anlDGU'.format(name2add[query], name2add[i[0]])).json()
-            json.dump(res, open(i[0]+'.json','w'))
+            json.dump(res, open('json/' + i[0]+'.json','w'))
         if res['rows'][0]['elements'][0]['status'] == 'OK':
             distance.append((i[0], res['rows'][0]['elements'][0]['distance']['value']))
     except Exception as e:
@@ -30,7 +29,8 @@ for index, (i, j) in enumerate(zip(tmp, distance)):
     if i[0] == j[0]:
         hybrid[i[0]] = hybrid.setdefault(i[0], 0) + index
     else:
-        hybrid[i[0]] = hybrid.setdefault(i[0], 0) + index*0.01
-        hybrid[j[0]] = hybrid.setdefault(j[0], 0) + index*0.99
+        hybrid[i[0]] = hybrid.setdefault(i[0], 0) + index*0.00001
+        hybrid[j[0]] = hybrid.setdefault(j[0], 0) + index*0.99999
 
 print(sorted(hybrid.items(), key=lambda x:-x[1])[:int(sys.argv[3])])
+
